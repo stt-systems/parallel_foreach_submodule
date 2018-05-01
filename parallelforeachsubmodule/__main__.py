c644d555350e565ca7597189a96ae1487118ac05
Rubén de Celis Hernández <rubendecelis@ingran.es>
2018-05-01 21:12:34 +0200

    Add entry point for pfs module

---
+++ parallelforeachsubmodule/__main__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Execute with:
+# $ python -m pfs (2.7+)
+
+import parallelforeachsubmodule.main
+
+if __name__ == "__main__":
+    parallelforeachsubmodule.main.main()
---
+++ parallelforeachsubmodule/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import parallelforeachsubmodule.pfs
+
+
+def main():
+    parallelforeachsubmodule.pfs.PFS().run()
