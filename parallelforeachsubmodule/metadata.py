39030eec31a96f6d0d6fba8d637466c80d10a1e8
rdecelis <rdecelis@stt-systems.com>
2018-12-11 10:09:30 +0100

    Add output_filter parameter in PFSProcess

--- parallelforeachsubmodule/metadata.py
+++ parallelforeachsubmodule/metadata.py
@@ -3,7 +3,7 @@
 
 class Metadata:
     def __init__(self):
-        self.__version__ = '0.4.5'
+        self.__version__ = '0.4.9'
         self.__author__ = 'Rubén de Celis Hernández'
 
     def get_version(self):
--- parallelforeachsubmodule/process.py
+++ parallelforeachsubmodule/process.py
@@ -5,21 +5,28 @@ import os
 
 
 class PFSProcess(object):
-    def __init__(self, submodule, path, cmd, counter):
+    def __init__(self, submodule, path, cmd, counter, output_filter=""):
         self.__submodule = submodule
         self.__path = path
         self.__cmd = cmd
         self.__counter = counter
+        self.__output_filter = output_filter
         self.__output = None
         self.__p = None
 
     def run(self):
         self.__output = "\n\n" + self.__submodule + "\n"
-        #self.__output = sub.check_output(self.__cmd)
+        # self.__output = sub.check_output(self.__cmd)
         self.__p = sub.Popen(self.__cmd, stdout=sub.PIPE, stderr=sub.PIPE, shell=True,
                              cwd=os.path.join(self.__path, self.__submodule))
-        self.__output += self.__p.communicate()[0].decode('utf-8')
-        if self.__p.communicate()[1]:
+
+        if self.__output_filter == "":
+            self.__output += self.__p.communicate()[0].decode('utf-8')  # stdoutdata
+        else:
+            if str(self.__p.communicate()[0].decode('utf-8')).find(self.__output_filter) != -1:
+                self.__output += self.__p.communicate()[0].decode('utf-8')
+
+        if self.__p.communicate()[1]:  # stderrdata
             self.__output += self.__p.communicate()[1].decode('utf-8')
 
         self.__output += str(self.__counter.increment_value())
