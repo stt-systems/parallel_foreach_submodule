c02b29426af201c8b95ca1d3c395bd77267f8f1b
rdecelis <rdecelis@stt-systems.com>
2019-11-21 12:56:21 +0100

    Add ✅ test_not_in_branch_shortcut

--- tests/test_pfs.py
+++ tests/test_pfs.py
@@ -129,6 +129,28 @@ class TestPFS(unittest.TestCase):
                                         '/tmp/git_submodules_test_project' + ' && pfs --in-branch develop --verbose',
                                         shell=True).decode('latin-1'))
 
+    def test_not_in_branch_shortcut(self):
+        if os.name == 'nt':  # on windows
+            print(self.sub.check_output('cd ' + os.path.dirname(os.path.abspath(__file__)) +
+                                        '\\tmp\\git_submodules_test_project' + ' && pfs -c "git checkout master"',
+                                        shell=True).decode('utf-8'))
+            print(self.sub.check_output('cd ' + os.path.dirname(os.path.abspath(__file__)) +
+                                        '\\tmp\\git_submodules_test_project\\submodules\\linearizator' +
+                                        ' && git checkout develop', shell=True).decode('utf-8'))
+            print(self.sub.check_output('cd ' + os.path.dirname(os.path.abspath(__file__)) +
+                                        '\\tmp\\git_submodules_test_project' + ' && pfs --not-in-branch develop --verbose',
+                                        shell=True).decode('latin-1'))
+        else:  # on linux / os x
+            print(self.sub.check_output('cd ' + os.path.dirname(os.path.abspath(__file__)) +
+                                        '/tmp/git_submodules_test_project' + ' && pfs -c "git checkout master"',
+                                        shell=True).decode('utf-8'))
+            print(self.sub.check_output('cd ' + os.path.dirname(os.path.abspath(__file__)) +
+                                        '/tmp/git_submodules_test_project/submodules/linearizator' +
+                                        ' && git checkout develop', shell=True).decode('utf-8'))
+            print(self.sub.check_output('cd ' + os.path.dirname(os.path.abspath(__file__)) +
+                                        '/tmp/git_submodules_test_project' + ' && pfs --not-in-branch develop --verbose',
+                                        shell=True).decode('latin-1'))
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
